6e8eab65af08db9a5897567a40c91c21dfd5bccc
MrTyger <31512139+MrTyger@users.noreply.github.com>
2018-09-07 22:35:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on('message', msg => {
             msg.reply('Je suis d\'accord avec toi.')
     }
     if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        msg.channel.send("http://magicfury.fr/")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
 
