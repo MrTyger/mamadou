27611b316828712e06cd7f75e570d41a9b6ed06f
MrTyger <31512139+MrTyger@users.noreply.github.com>
2018-09-28 18:51:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg = require('./index.json');
+const token = process.env.token;
 const prefix = ("?");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+    bot.user.setActivity('MagicFury').catch(console.error)
 });
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Wesh Wesh' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
@@ -29,14 +30,21 @@ bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")
     }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
+    if (msg.content.match(/Dieu/i)) {
+            msg.reply('MrTyger?.')
+    }
+    if (msg.content.match(/je suis idiot/i)) {
+            msg.reply('Hooo ouiii !.')
+    }
+    if (msg.content.match(/test/i)) {
+            member.createDM().then(channel => {
+        return channel.send('Ty Gueule' + member.displayName)
     }
     if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        msg.channel.send("http://magicfury.fr/")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
 
 });
 
-bot.login(cfg.token);
+bot.login(token);
